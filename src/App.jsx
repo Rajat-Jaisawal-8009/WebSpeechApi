8b17a2cdaeb5689930307f18b4e1cdefde44ee17
Rajat Jaiswal <rajatsss9@gmail.com>
2025-10-01 03:38:27 +0530

    “yr”

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,16 @@ function App() {
     return <span>⚠️ Browser Speech Recognition support नहीं करता।</span>;
   }
 
+   let silenceTimer;
+  
+ const resetSilenceTimer = () => {
+    if (silenceTimer) clearTimeout(silenceTimer);
+    silenceTimer = setTimeout(() => {
+   SpeechRecognition.stopListening();
+   
+    }, 4000);
+  };
+
   const startListening = () =>
     SpeechRecognition.startListening({
       continuous: true,   // लगातार सुनता रहेगा
@@ -20,12 +30,25 @@ function App() {
       language: "en-US",
     });
 
+    if(recognition){
+  const originalOnResult = recognition.onresult;
+  recognition.onresult = (event) => {
+    if (silenceTimer) clearTimeout(silenceTimer);
+        if (typeof originalOnResult === "function") {
+        originalOnResult(event)
+        console.log("afhkahfgkhg")
+         resetSilenceTimer(); 
+        }
+   
+  };
+}
+resetSilenceTimer()
   return (
     <div style={{ padding: "20px", textAlign: "center" }}>
-      <h2>🎤 React Speech Recognition Demo</h2>
+      <h2>🎤 React Speech Recognition De</h2>
       <p><b>Status:</b> {listening ? "Listening..." : "Stopped"}</p>
 
-      <button onClick={startListening}>Start</button>
+      <button onClick={startListening}>Start???</button>
       <button onClick={SpeechRecognition.stopListening}>Stop</button>
       <button onClick={resetTranscript}>Reset</button>
 
